fe850eabb1beb41ac4e4ac9fcc5c501ce0b61f07
NoahIsMe <79447673+NoahRamones@users.noreply.github.com>
2023-10-28 01:05:36 -0700

    added landing page

---
+++ myapp/src/landingpage.js
@@ -0,0 +1,15 @@
+// src/components/LandingPage.js
+
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Product/Service</h1>
+      <p>Here's a little about what we do...</p>
+      <button>Learn More</button>
+    </div>
+  );
+};
+
+export default LandingPage;
